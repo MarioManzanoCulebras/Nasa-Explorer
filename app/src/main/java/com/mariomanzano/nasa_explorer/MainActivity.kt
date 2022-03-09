3545dd7b6687acb2d28f18c26ce15db44ac5dce3
Mario Manzano <mario.manzano@experis.es>
2022-03-09 14:27:54 +0100

    Configure the proyect to use JetpackCompose and added libraries needed.

--- app/src/main/java/com/mariomanzano/nasa_explorer/MainActivity.kt
+++ app/src/main/java/com/mariomanzano/nasa_explorer/MainActivity.kt
@@ -16,28 +16,19 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NasaexplorerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+          NasaExploreApp {
+
+          }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
+fun NasaExploreApp(content: @Composable ()-> Unit) {
     NasaexplorerTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+            content()
+        }
     }
 }
