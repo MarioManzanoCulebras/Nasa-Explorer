06c1bac01154995dd248f173d05d5a072e8ce621
Mario Manzano <bross1112@gmail.com>
2022-04-09 18:35:45 +0200

    Updated palette colors.

--- app/src/main/java/com/mariomanzano/nasaexplorer/ui/theme/Color.kt
+++ app/src/main/java/com/mariomanzano/nasaexplorer/ui/theme/Color.kt
@@ -6,3 +6,10 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Blue = Color(0xFF105BD8)
+val BlueDark = Color(0xFF0B3D91)
+val Red = Color(0xFFDD361C)
+val RedDark = Color(0xFF99231B)
+val Purple = Color(0XFF4C2C92)
+
