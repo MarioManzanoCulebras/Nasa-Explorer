e58796f04f235ee92edcc17e58dd6a7e37c33b00
Mario Manzano <bross1112@gmail.com>
2022-04-23 20:03:47 +0200

    Added new font family: Montserrat

--- app/src/main/java/com/mariomanzano/nasaexplorer/ui/theme/Type.kt
+++ app/src/main/java/com/mariomanzano/nasaexplorer/ui/theme/Type.kt
@@ -9,15 +9,36 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.mariomanzano.nasaexplorer.R
 
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_black, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.montserrat_blackitalic, FontWeight.Black, FontStyle.Italic),
+    Font(R.font.montserrat_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.montserrat_bolditalic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.montserrat_extrabold, FontWeight.ExtraBold, FontStyle.Normal),
+    Font(R.font.montserrat_extrabolditalic, FontWeight.ExtraBold, FontStyle.Italic),
+    Font(R.font.montserrat_extralight, FontWeight.ExtraLight, FontStyle.Normal),
+    Font(R.font.montserrat_extralightitalic, FontWeight.ExtraLight, FontStyle.Italic),
+    Font(R.font.montserrat_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.montserrat_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.montserrat_lightitalic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.montserrat_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.montserrat_mediumitalic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.montserrat_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.montserrat_semibold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.montserrat_semibolditalic, FontWeight.SemiBold, FontStyle.Italic),
+    Font(R.font.montserrat_thin, FontWeight.Thin, FontStyle.Normal),
+    Font(R.font.montserrat_thinitalic, FontWeight.Thin, FontStyle.Italic),
+)
+
 val GrandStander = FontFamily(
-Font(R.font.grandstander_black, FontWeight.Black, FontStyle.Normal),
-Font(R.font.grandstander_blackitalic, FontWeight.Black, FontStyle.Italic),
-Font(R.font.grandstander_bold, FontWeight.Bold, FontStyle.Normal),
-Font(R.font.grandstander_bolditalic, FontWeight.Bold, FontStyle.Italic),
-Font(R.font.grandstander_extrabold, FontWeight.ExtraBold, FontStyle.Normal),
-Font(R.font.grandstander_extrabolditalic, FontWeight.ExtraBold, FontStyle.Italic),
-Font(R.font.grandstander_extralight, FontWeight.ExtraLight, FontStyle.Normal),
-Font(R.font.grandstander_extralightitalic, FontWeight.ExtraLight, FontStyle.Italic),
+    Font(R.font.grandstander_black, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.grandstander_blackitalic, FontWeight.Black, FontStyle.Italic),
+    Font(R.font.grandstander_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.grandstander_bolditalic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.grandstander_extrabold, FontWeight.ExtraBold, FontStyle.Normal),
+    Font(R.font.grandstander_extrabolditalic, FontWeight.ExtraBold, FontStyle.Italic),
+    Font(R.font.grandstander_extralight, FontWeight.ExtraLight, FontStyle.Normal),
+    Font(R.font.grandstander_extralightitalic, FontWeight.ExtraLight, FontStyle.Italic),
 Font(R.font.grandstander_italic, FontWeight.Normal, FontStyle.Italic),
 Font(R.font.grandstander_light, FontWeight.Light, FontStyle.Normal),
 Font(R.font.grandstander_lightitalic, FontWeight.Light, FontStyle.Italic),
@@ -32,7 +53,7 @@ Font(R.font.grandstander_thinitalic, FontWeight.Thin, FontStyle.Italic),
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    defaultFontFamily = GrandStander,
+    defaultFontFamily = Montserrat,
     body1 = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
