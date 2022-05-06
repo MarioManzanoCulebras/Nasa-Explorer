0c76a051f05479580d138fa278e54b893f821322
Mario Manzano <bross1112@gmail.com>
2022-05-06 16:13:08 +0200

    Updated class test to use MockitoJUnitRunner

--- usecases/src/test/java/com/mariomanzano/nasaexplorer/usecases/SwitchItemToFavoriteUseCaseTest.kt
+++ usecases/src/test/java/com/mariomanzano/nasaexplorer/usecases/SwitchItemToFavoriteUseCaseTest.kt
@@ -4,25 +4,40 @@ import com.mariomanzano.nasaexplorer.repositories.DailyEarthRepository
 import com.mariomanzano.nasaexplorer.repositories.DailyPicturesRepository
 import com.mariomanzano.nasaexplorer.repositories.MarsRepository
 import kotlinx.coroutines.runBlocking
+import org.junit.Before
 import org.junit.Test
-import org.mockito.kotlin.mock
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 
+@RunWith(MockitoJUnitRunner::class)
 class SwitchItemToFavoriteUseCaseTest {
+
+    @Mock
+    lateinit var dailyPicturesRepository: DailyPicturesRepository
+
+    @Mock
+    lateinit var dailyEarthRepository: DailyEarthRepository
+
+    @Mock
+    lateinit var marsRepository: MarsRepository
+
+    private lateinit var switchItemToFavoriteUseCase: SwitchItemToFavoriteUseCase
+
+    @Before
+    fun setUp() {
+        switchItemToFavoriteUseCase = SwitchItemToFavoriteUseCase(
+            dailyPicturesRepository,
+            dailyEarthRepository,
+            marsRepository
+        )
+    }
 
     @Test
     fun `Invoke calls POD repository`(): Unit = runBlocking {
         val dailyPictureItem = samplePOD.copy(id = 1)
-        val dailyPicturesRepository = mock<DailyPicturesRepository>()
-        val dailyEarthRepository = mock<DailyEarthRepository>()
-        val marsRepository = mock<MarsRepository>()
-        val switchItemToFavoriteUseCase =
-            SwitchItemToFavoriteUseCase(
-                dailyPicturesRepository,
-                dailyEarthRepository,
-                marsRepository
-            )
 
         switchItemToFavoriteUseCase(dailyPictureItem)
 
@@ -32,15 +47,6 @@ class SwitchItemToFavoriteUseCaseTest {
     @Test
     fun `Invoke calls Earth repository`(): Unit = runBlocking {
         val earthItem = sampleEarth.copy(id = 1)
-        val dailyPicturesRepository = mock<DailyPicturesRepository>()
-        val dailyEarthRepository = mock<DailyEarthRepository>()
-        val marsRepository = mock<MarsRepository>()
-        val switchItemToFavoriteUseCase =
-            SwitchItemToFavoriteUseCase(
-                dailyPicturesRepository,
-                dailyEarthRepository,
-                marsRepository
-            )
 
         switchItemToFavoriteUseCase(earthItem)
 
@@ -50,15 +56,6 @@ class SwitchItemToFavoriteUseCaseTest {
     @Test
     fun `Invoke calls Mars repository`(): Unit = runBlocking {
         val marsItem = sampleMars.copy(id = 1)
-        val dailyPicturesRepository = mock<DailyPicturesRepository>()
-        val dailyEarthRepository = mock<DailyEarthRepository>()
-        val marsRepository = mock<MarsRepository>()
-        val switchItemToFavoriteUseCase =
-            SwitchItemToFavoriteUseCase(
-                dailyPicturesRepository,
-                dailyEarthRepository,
-                marsRepository
-            )
 
         switchItemToFavoriteUseCase(marsItem)
 
