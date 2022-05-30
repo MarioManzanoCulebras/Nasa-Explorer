7aa94d04f1bfd988c81f5a25cdb5c392d5692f34
Mario Manzano <bross1112@gmail.com>
2022-05-30 09:48:26 +0200

    Substracted more days to MarsService to fix server no data.

--- app/src/main/java/com/mariomanzano/nasaexplorer/network/MarsServerDataSource.kt
+++ app/src/main/java/com/mariomanzano/nasaexplorer/network/MarsServerDataSource.kt
@@ -21,7 +21,7 @@ class MarsServerDataSource @Inject constructor(
                         (Calendar.getInstance().apply {
                             set(
                                 Calendar.DAY_OF_MONTH,
-                                Calendar.getInstance().get(Calendar.DAY_OF_MONTH) - 59
+                                Calendar.getInstance().get(Calendar.DAY_OF_MONTH) - 120
                             )
                         }).time
                     )
