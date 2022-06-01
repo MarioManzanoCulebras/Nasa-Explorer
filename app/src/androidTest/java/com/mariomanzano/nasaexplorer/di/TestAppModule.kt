da889786ea55a5f7e19096d6fa6ab422e3df15a1
Mario Manzano <bross1112@gmail.com>
2022-06-01 14:10:46 +0200

    Added Hilt Injections to TestAppModule in order to inject the viewModel on the screen using Hilt to run with the mock server and fix on the class test.

--- app/src/androidTest/java/com/mariomanzano/nasaexplorer/di/TestAppModule.kt
+++ app/src/androidTest/java/com/mariomanzano/nasaexplorer/di/TestAppModule.kt
@@ -3,11 +3,17 @@ package com.mariomanzano.nasaexplorer.di
 import android.app.Application
 import androidx.room.Room
 import com.mariomanzano.nasaexplorer.R
-import com.mariomanzano.nasaexplorer.data.database.NasaDatabase
+import com.mariomanzano.nasaexplorer.data.database.*
 import com.mariomanzano.nasaexplorer.data.di.ApiEndPoint
 import com.mariomanzano.nasaexplorer.data.di.ApiKey
+import com.mariomanzano.nasaexplorer.data.di.AppDataModule
 import com.mariomanzano.nasaexplorer.data.di.AppModule
-import com.mariomanzano.nasaexplorer.network.QueryInterceptor
+import com.mariomanzano.nasaexplorer.datasource.*
+import com.mariomanzano.nasaexplorer.network.*
+import com.mariomanzano.nasaexplorer.repositories.DailyPicturesRepository
+import com.mariomanzano.nasaexplorer.repositories.LastDbUpdateRepository
+import com.mariomanzano.nasaexplorer.ui.screens.dailypicture.DailyPictureViewModel
+import com.mariomanzano.nasaexplorer.usecases.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.components.SingletonComponent
@@ -16,12 +22,13 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
 
 @Module
 @TestInstallIn(
     components = [SingletonComponent::class],
-    replaces = [AppModule::class]
+    replaces = [AppModule::class, AppDataModule::class]
 )
 object TestAppModule {
 
@@ -43,7 +50,7 @@ object TestAppModule {
 
     @Provides
     @ApiEndPoint
-    fun providesApiEndPoint(app: Application): String = "http://localhost:8080"
+    fun providesApiEndPoint(): String = "http://localhost:8080"
 
     @Provides
     fun provideLoggingInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
@@ -66,4 +73,89 @@ object TestAppModule {
             .addConverterFactory(GsonConverterFactory.create())
             .client(okHttpClient)
             .build()
+
+    @Provides
+    fun provideDailyPictureService(restAdapter: Retrofit): DailyPicturesService =
+        restAdapter.create()
+
+    @Provides
+    fun provideDailyEarthServiceService(restAdapter: Retrofit): DailyEarthService =
+        restAdapter.create()
+
+    @Provides
+    fun provideMarsService(restAdapter: Retrofit): MarsService = restAdapter.create()
+
+    @Provides
+    fun provideDailyPictureViewModel(
+        getPODUseCase: GetPODUseCase,
+        requestPODListUseCase: RequestPODListUseCase,
+        requestPODSingleDayUseCase: RequestPODSingleDayUseCase,
+        getLastPODUpdateDateUseCase: GetLastPODUpdateDateUseCase,
+        updateLastPODUpdateUseCase: UpdateLastPODUpdateUseCase
+    ): DailyPictureViewModel =
+        DailyPictureViewModel(
+            getPODUseCase,
+            requestPODListUseCase,
+            requestPODSingleDayUseCase,
+            getLastPODUpdateDateUseCase,
+            updateLastPODUpdateUseCase
+        )
+
+    @Provides
+    fun provideGetPODUseCase(repository: DailyPicturesRepository): GetPODUseCase =
+        GetPODUseCase(repository)
+
+    @Provides
+    fun provideRequestPODListUseCase(repository: DailyPicturesRepository): RequestPODListUseCase =
+        RequestPODListUseCase(repository)
+
+    @Provides
+    fun providePODSingleDayUseCase(repository: DailyPicturesRepository): RequestPODSingleDayUseCase =
+        RequestPODSingleDayUseCase(repository)
+
+    @Provides
+    fun provideGetLastPODUpdateUseCase(repository: LastDbUpdateRepository): GetLastPODUpdateDateUseCase =
+        GetLastPODUpdateDateUseCase(repository)
+
+    @Provides
+    fun provideLastDbUpdateRepository(dataSource: LastDbUpdateRoomDataSource): LastDbUpdateRepository =
+        LastDbUpdateRepository(dataSource)
+
+    @Provides
+    fun provideLastDbUpdateDataSource(db: NasaDatabase): LastDbUpdateDataSource =
+        LastDbUpdateRoomDataSource(db.nasaDao())
+
+    @Provides
+    fun provideUpdateLastPODUpdateUseCase(repository: LastDbUpdateRepository): UpdateLastPODUpdateUseCase =
+        UpdateLastPODUpdateUseCase(repository)
+
+    @Provides
+    fun provideDailyPicturesRepository(
+        local: PODLocalDataSource,
+        remote: PODRemoteDataSource
+    ): DailyPicturesRepository = DailyPicturesRepository(local, remote)
+
+    @Provides
+    fun providePODLocalDataSource(db: NasaDatabase): PODLocalDataSource =
+        PODRoomDataSource(db.nasaDao())
+
+    @Provides
+    fun provideEarthLocalDataSource(db: NasaDatabase): EarthLocalDataSource =
+        EarthRoomDataSource(db.nasaDao())
+
+    @Provides
+    fun provideMarsLocalDataSource(db: NasaDatabase): MarsLocalDataSource =
+        MarsRoomDataSource(db.nasaDao())
+
+    @Provides
+    fun providePODRemoteDataSource(dailyPictureService: DailyPicturesService): PODRemoteDataSource =
+        PODServerDataSource(dailyPictureService)
+
+    @Provides
+    fun provideEarthRemoteDataSource(dailyEarthService: DailyEarthService): EarthRemoteDataSource =
+        EarthServerDataSource(dailyEarthService)
+
+    @Provides
+    fun provideMarsRemoteDataSource(marsService: MarsService): MarsRemoteDataSource =
+        MarsServerDataSource(marsService)
 }
--- app/src/androidTest/java/com/mariomanzano/nasaexplorer/ui/NasaItemsListScreenTest.kt
+++ app/src/androidTest/java/com/mariomanzano/nasaexplorer/ui/NasaItemsListScreenTest.kt
@@ -5,68 +5,89 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.platform.app.InstrumentationRegistry
-import com.mariomanzano.domain.entities.PictureOfDayItem
 import com.mariomanzano.nasaexplorer.R
-import com.mariomanzano.nasaexplorer.ui.screens.common.NasaItemsListScreen
+import com.mariomanzano.nasaexplorer.data.server.MockWebServerRule
+import com.mariomanzano.nasaexplorer.data.server.fromJson
+import com.mariomanzano.nasaexplorer.ui.screens.dailypicture.DailyPictureScreen
+import com.mariomanzano.nasaexplorer.ui.screens.dailypicture.DailyPictureViewModel
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import okhttp3.OkHttpClient
+import okhttp3.mockwebserver.MockResponse
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import java.util.*
+import javax.inject.Inject
 
+@ExperimentalCoroutinesApi
 @ExperimentalFoundationApi
 @ExperimentalMaterialApi
+@HiltAndroidTest
 class NasaItemsListScreenTest {
 
-    @get:Rule
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val mockWebServerRule = MockWebServerRule()
+
+    @get:Rule(order = 2)
     val composeTestRule = createComposeRule()
 
     private val ctx = InstrumentationRegistry.getInstrumentation().targetContext
 
-    private val items: List<PictureOfDayItem> = (1..100).map {
-        PictureOfDayItem(
-            id = it,
-            date = Calendar.getInstance().apply { add(Calendar.DAY_OF_MONTH, it) },
-            title = "Title $it",
-            description = "Description $it",
-            url = "",
-            mediaType = "image",
-            favorite = false,
-            copyRight = "copyRight"
-        )
-    }
+    @Inject
+    lateinit var okHttpClient: OkHttpClient
+
+    @Inject
+    lateinit var dailyPictureViewModel: DailyPictureViewModel
 
     @Before
     fun setUp() {
+        mockWebServerRule.server.enqueue(
+            MockResponse().fromJson("astronomy_picture_of_day_6_days_request.json")
+        )
+
+        hiltRule.inject()
+
+        val resource = OkHttp3IdlingResource.create("OkHttp", okHttpClient)
+        IdlingRegistry.getInstance().register(resource)
+
         composeTestRule.setContent {
-            NasaItemsListScreen(
-                items = items,
+
+            DailyPictureScreen(
+                listState = rememberLazyListState(),
                 onClick = {},
                 onItemsMoreClicked = {},
-                listState = rememberLazyListState()
+                viewModel = dailyPictureViewModel
             )
         }
     }
 
     @Test
-    fun navigatesTo51(): Unit = with(composeTestRule) {
-        onNode(hasScrollToIndexAction()).performScrollToIndex(25)
-        onNodeWithText("Title 51").assertExists()
+    fun navigatesTo4(): Unit = with(composeTestRule) {
+        onRoot().printToLog("semanticTree")
+        onNode(hasScrollToIndexAction()).performScrollToIndex(4)
+        Thread.sleep(5000)
+        onNodeWithText("Orion over Green Bank").assertExists()
     }
 
     @Test
-    fun navigatesTo51AndShowsBottomSheet(): Unit = with(composeTestRule) {
-        onNode(hasScrollToIndexAction()).performScrollToIndex(25)
+    fun navigatesTo6AndShowsBottomSheet(): Unit = with(composeTestRule) {
+        onRoot().printToLog("semanticTree")
+        onNode(hasScrollToIndexAction()).performScrollToIndex(6)
+        Thread.sleep(5000)
         onNode(
-            hasParent(hasText("Title 51")) and
+            hasParent(hasText("Aurora Over White Dome Geyser")) and
                     hasContentDescription(ctx.getString(R.string.more_options))
         ).performClick()
-
+        Thread.sleep(5000)
         onNode(
             hasAnySibling(hasText(ctx.getString(R.string.go_to_detail))) and
-                    hasText("Title 51")
+                    hasText("Aurora Over White Dome Geyser", true)
         ).assertExists()
-
-        onRoot(useUnmergedTree = true).printToLog("semanticTree")
     }
 }
